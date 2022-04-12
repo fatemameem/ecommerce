7293e6f38f7fb83a64bd276dd26f0b8d663a492d
fatemameem <48911993+fatemameem@users.noreply.github.com>
2022-04-13 04:12:17 +0600

    component skeletons added

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Cart from "../Cart/Cart";
+import Product from "../Products/Product";
+import "./Shop.css";
+
+const Shop = () => {
+  return (
+    <div>
+      <Product></Product>
+      <Cart></Cart>
+    </div>
+  );
+};
+export default Shop;
